f0503e2499e6cc63c10a1d4242554a8423541b98
Stanislav <stanislav@pikkatech.eu>
2025-06-23 20:18:17 +0200

    Removed Gui and Chrono folders; still Framework version

--- Code/CSharp/Factotum.Tests/Program.cs
+++ Code/CSharp/Factotum.Tests/Program.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Factotum.Chrono;
-using Factotum.Gui.Chrono.Dialogs;
 
 namespace Factotum.Tests
 {
@@ -12,12 +10,7 @@ namespace Factotum.Tests
 	{
 		static void Main(string[] args)
 		{
-			TemporalDialog dialog = new TemporalDialog();
 
-			if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-			{
-
-			}
 		}
 	}
 }
