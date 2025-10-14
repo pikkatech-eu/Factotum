9e1bc6888ee61f1f44bf4c188a41180f26d140f3
Stanislav <stanislav@pikkatech.eu>
2025-10-14 15:11:41 +0200

    StringConversionExtensions; Version.cs

---
+++ Code/CSharp/Factotum/Text/StringConversionExtensions.cs
@@ -0,0 +1,68 @@
+﻿/***********************************************************************************
+* File:         ParsingTools.cs                                                    *
+* Contents:     Class ParsingTools                                                 *
+* Author:       Stanislav "Bav" Koncebovski (stanislav@pikkatech.eu)               *
+* Date:         2024-09-13 10:31                                                   *
+* Version:      1.0                                                                *
+* Copyright:    pikkatech.eu (www.pikkatech.eu)                                    *
+***********************************************************************************/
+
+namespace Factotum.Tools
+{
+	/// <summary>
+	/// Safely converts a string to a typed numeric value.
+	/// </summary>
+	public static class StringConversionExtensions
+	{
+		/// <summary>
+		/// Performs the conversion.
+		/// </summary>
+		/// <typeparam name="T">
+		///		The type of the element to conver to.
+		///		At the moment, supports the following types:
+		///			int
+		///			double
+		///	</typeparam>
+		/// <param name="x">The text to convert.</param>
+		/// <param name="defaultValue">Default value to assign.</param>
+		/// <returns>The typed value as converted, if successful, otherwise the default value.</returns>
+		public static T ToNumber<T>(this string x, T defaultValue = default(T))
+		{
+			Type type = typeof(T);
+
+			switch (type)
+			{
+				case Type _ when type == typeof(int):
+					return (T)Convert.ChangeType(Int32.TryParse(x, out int intValue) ? intValue : default(int), typeof(T));
+
+				case Type _ when type == typeof(short):
+					return (T)Convert.ChangeType(Int16.TryParse(x, out short shortValue) ? shortValue : default(short), typeof(T));
+
+				case Type _ when type == typeof(long):
+					return (T)Convert.ChangeType(Int64.TryParse(x, out long longValue) ? longValue : default(long), typeof(T));
+
+				case Type _ when type == typeof(byte):
+					return (T)Convert.ChangeType(Byte.TryParse(x, out byte byteValue) ? byteValue : default(byte), typeof(T));
+
+				case Type _ when type == typeof(bool):
+					return (T)Convert.ChangeType(Boolean.TryParse(x, out bool boolValue) ? boolValue : default(bool), typeof(T));
+
+				case Type _ when type == typeof(double):
+					return (T)Convert.ChangeType(Double.TryParse(x, out double doubleValue) ? doubleValue : default(double), typeof(T));
+
+				case Type _ when type == typeof(float):
+					return (T)Convert.ChangeType(Double.TryParse(x, out double floatValue) ? floatValue : default(float), typeof(T));
+
+				case Type _ when type == typeof(Guid):
+					return (T)Convert.ChangeType(Guid.TryParse(x, out Guid guidValue) ? guidValue : default(Guid), typeof(T));
+
+				case Type _ when type == typeof(DateTime):
+					return (T)Convert.ChangeType(DateTime.TryParse(x, out DateTime dateTimeValue) ? dateTimeValue : default(DateTime), typeof(T));
+
+
+				default:
+					return default(T);
+			}
+		}
+	}
+}
---
+++ Code/CSharp/Factotum/Versioning/Version.cs
@@ -0,0 +1,71 @@
+﻿/***********************************************************************************
+* File:         Version.cs                                                         *
+* Contents:     Class Version                                                      *
+* Author:       Stanislav "Bav" Koncebovski (stanislav@pikkatech.eu)               *
+* Date:         2025-09-29 17:06                                                   *
+* Version:      1.0                                                                *
+* Copyright:    pikkatech.eu (www.pikkatech.eu)                                    *
+***********************************************************************************/
+
+using Factotum.Toml;
+using Factotum.Tools;
+
+namespace Factotum.Versioning
+{
+	public class Version
+	{
+		private static readonly string DEFAULT_COMPANY = "Pikkatech";
+		private const string DEFAULT_VERSION_FILE_NAME = "version.toml";
+
+		#region Properties
+		public string	Company		{get;set;}	= DEFAULT_COMPANY;
+		public string	Product		{get;set;}	= "";
+
+		public int		Major		{get;set;}	= 0;
+		public int		Minor		{get;set;}	= 0;
+		public int		Build		{get;set;}	= 0;
+		public int		Revision	{get;set;}	= 0;
+
+		public string	Stage		{get;set;} = "";
+		public string	StageIndex	{get;set;} = "";
+		#endregion
+
+		#region Construction
+		public Version()
+		{
+			DateTime now	= DateTime.Now;
+
+			this.Major		= now.Year;
+			this.Minor		= now.Month;
+			this.Build		= now.Day;
+			this.Revision	= 0;
+		}
+
+		public void FromToml(string fileName = DEFAULT_VERSION_FILE_NAME)
+		{
+			string currentFolder = Directory.GetCurrentDirectory();
+
+			string currentVersionFile = Path.Combine(currentFolder, fileName);
+
+			if (File.Exists(currentVersionFile))
+			{
+				Tomler tomler = new Tomler();
+
+				tomler.Load(currentVersionFile);
+
+				this.Major		= tomler.GetValue("Version", "Major").ToNumber<int>();
+				this.Minor		= tomler.GetValue("Version", "Minor").ToNumber<int>();
+				this.Build		= tomler.GetValue("Version", "Minor").ToNumber<int>();
+				this.Revision	= tomler.GetValue("Version", "Revision").ToNumber<int>();
+			}
+		}
+		#endregion
+
+		#region String Representation
+		public override string ToString()
+		{
+			return $"{this.Major}.{this.Minor}.{this.Build}.{this.Revision}";
+		}
+		#endregion
+	}
+}
