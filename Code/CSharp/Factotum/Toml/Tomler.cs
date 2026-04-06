5c54b4cba5b6cdf1f251c2217dc51f4b86ab9909
Stanislav <stanislav@pikkatech.eu>
2026-04-06 17:48:24 +0200

    cosmetic changes

--- Code/CSharp/Factotum/Toml/Tomler.cs
+++ Code/CSharp/Factotum/Toml/Tomler.cs
@@ -7,13 +7,6 @@
 * Copyright:    pikkatech.eu (www.pikkatech.eu)                                    *
 ***********************************************************************************/
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Collections.Specialized.BitVector32;
-
 namespace Factotum.Toml
 {
 	/// <summary>
