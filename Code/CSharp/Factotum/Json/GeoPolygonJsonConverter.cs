0a35323960436c0d002dcd1d5b074d1c360a576e
Stanislav <stanislav@pikkatech.eu>
2026-06-19 23:55:44 +0200

    Code\CSharp\Factotum\Json\GeoPolygonJsonConverter.cs

---
+++ Code/CSharp/Factotum/Json/GeoPolygonJsonConverter.cs
@@ -0,0 +1,125 @@
+﻿/***********************************************************************************
+* File:         GeoPolygonJsonConverter.cs                                         *
+* Contents:     Class GeoPolygonJsonConverter                                      *
+* Author:       Alexander Konnen (alex@pikkatech.eu)                               *
+* Date:         2026-06-19 22:27                                                   *
+* Version:      1.0                                                                *
+* Copyright:    pikkatech.eu (www.pikkatech.eu)                                    *
+***********************************************************************************/
+
+using System.Text.Json;
+using Factotum.Maths.Geospatial;
+
+namespace Factotum.Json
+{
+	/// <summary>
+	/// Converts GeoPolygons to and from JSON.
+	/// </summary>
+	public static class GeoPolygonJsonConverter
+	{
+		#region Public features
+		/// <summary>
+		/// Converts a JSON string to a GeoPolygon.
+		/// Handles the OSM-geometry JSON format ("format=geojson" in the query).
+		/// </summary>
+		/// <param name="json">The JSON string.</param>
+		/// <returns>Geopolygon converted, if successful, otherwise null.</returns>
+		public static GeoPolygon FromJson(string json)
+		{
+			var result = FromJsonSingle(json);
+
+			if (result != null)
+			{
+				return result;
+			}
+			else
+			{
+				result = FromJsonMulti(json);
+
+				if (result != null)
+				{
+					return result;
+				}
+				else
+				{
+					return null;
+				}
+			}
+		}
+		#endregion
+
+		#region Private Auxiliary
+		/// <summary>
+		/// Tries to extract a polygon from a single-polygon structure.
+		/// </summary>
+		/// <param name="json">The JSON string to extract from.</param>
+		/// <returns>An instance of GeoPolygon, if successful, otherwise null.</returns>
+		private static GeoPolygon FromJsonSingle(string json)
+		{
+			try
+			{
+				var myDeserializedClass = JsonSerializer.Deserialize<RootSingle>(json);
+				var geometry			= myDeserializedClass.features[0].geometry;
+
+				return ExtractSinglePolygon(geometry.coordinates[0]);
+			}
+			catch (Exception)
+			{
+				return null;
+			}
+		}
+
+		/// <summary>
+		///		Tries to extract a polygon from a multi-polygon structure.
+		///		Extracts a list of polygons and selects the biggest by surface.
+		/// </summary>
+		/// <param name="json">The JSON string to extract from.</param>
+		/// <returns>An instance of GeoPolygon, if successful, otherwise null.</returns>
+		private static GeoPolygon FromJsonMulti(string json)
+		{
+			try
+			{
+				var myDeserializedClass = JsonSerializer.Deserialize<RootMulti>(json);
+
+				var geometry	= myDeserializedClass.features[0].geometry;
+
+				List<GeoPolygon> polygons	= new List<GeoPolygon>();
+
+				foreach (var subArray in geometry.coordinates[0])
+				{
+					GeoPolygon pg = ExtractSinglePolygon(subArray);
+
+					polygons.Add(pg);
+				}
+
+				return polygons.MaxBy(p=>p.Surface);
+			}
+			catch (Exception)
+			{
+				return null;
+			}
+		}
+
+		/// <summary>
+		/// Extracts a single polygon from a double list of doubles.
+		/// </summary>
+		/// <param name="coordinates">The double list of doubles treated as coordinate pairs.</param>
+		/// <returns>An instance of GeoPolygon extracted.</returns>
+		private static GeoPolygon ExtractSinglePolygon(List<List<double>> coordinates)
+		{
+			GeoPolygon polygon	= new GeoPolygon();
+
+			foreach (var pair in coordinates)
+			{
+				double latitude		= pair[0];
+				double longitude	= pair[1];
+
+				GeoPoint geoPoint	= new GeoPoint(latitude, longitude);
+				polygon._vertices.Add(geoPoint);
+			}
+
+			return polygon;
+		}
+		#endregion
+	}
+}
---
+++ Code/CSharp/Factotum/Json/JsonGeoPolygonAuxiliary.cs
@@ -0,0 +1,112 @@
+﻿/***********************************************************************************
+* File:         JsonGeoPolygonAuxiliary.cs                                         *
+* Contents:     Auxiliary Classes for GeoPolygonJsonConverter                      *
+* Author:       Alexander Konnen (alex@pikkatech.eu)                               *
+* Date:         2026-06-19 23:46                                                   *
+* Version:      1.0                                                                *
+* Copyright:    pikkatech.eu (www.pikkatech.eu)                                    *
+***********************************************************************************/
+
+// RootSingle myDeserializedClass = JsonConvert.DeserializeObject<RootSingle>(myJsonResponse);
+
+/// <summary>
+/// Class Address.
+/// Added to satgisfy JsonSerializer.
+/// Not used directly.
+/// </summary>
+public class Address
+{
+	public string city { get; set; }
+	public string state { get; set; }
+	public string ISO31662lvl4 { get; set; }
+	public string country { get; set; }
+	public string country_code { get; set; }
+}
+
+/// <summary>
+/// Class Properties.
+/// Added to satgisfy JsonSerializer.
+/// Not used directly.
+/// </summary>
+public class Properties
+{
+	public int place_id { get; set; }
+	public string osm_type { get; set; }
+	public int osm_id { get; set; }
+	public int place_rank { get; set; }
+	public string category { get; set; }
+	public string type { get; set; }
+	public double importance { get; set; }
+	public string addresstype { get; set; }
+	public string name { get; set; }
+	public string display_name { get; set; }
+	public Address address { get; set; }
+}
+
+/// <summary>
+/// Geometry in single format.
+/// Saves points as a 3-fold list of doubles.
+/// </summary>
+public class GeometrySingle
+{
+	public string type { get; set; }
+	public List<List<List<double>>> coordinates { get; set; }
+}
+
+/// <summary>
+/// Geometry in multi format.
+/// Saves points as a 4-fold list of doubles.
+/// </summary>
+public class GeometryMulti
+{
+	public string type { get; set; }
+	public List<List<List<List<double>>>> coordinates { get; set; }
+}
+
+/// <summary>
+/// Feature in single format.
+/// Uses GeometrySingle as geometry representation.
+/// </summary>
+public class FeatureSingle
+{
+	public string type { get; set; }
+	public Properties properties { get; set; }
+	public List<double> bbox { get; set; }
+	public GeometrySingle geometry { get; set; }
+}
+
+/// <summary>
+/// Feature in multi format.
+/// Uses GeometryMulti as geometry representation.
+/// </summary>
+public class FeatureMulti
+{
+	public string type { get; set; }
+	public Properties properties { get; set; }
+	public List<double> bbox { get; set; }
+	public GeometryMulti geometry { get; set; }
+}
+
+/// <summary>
+/// Root in single format.
+/// Uses FeatureSingle as feature representation.
+/// </summary>
+public class RootSingle
+{
+	public string type { get; set; }
+	public string licence { get; set; }
+	public List<FeatureSingle> features { get; set; }
+}
+
+/// <summary>
+/// Root in multi format.
+/// Uses FeatureMulti as feature representation.
+/// </summary>
+public class RootMulti
+{
+	public string type { get; set; }
+	public string licence { get; set; }
+	public List<FeatureMulti> features { get; set; }
+}
+
+
